086ac1eef7492e90b365c99b639603a4678a518d
IsdbSscDev <sscdeveloper@isdb.org>
2022-08-03 17:14:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          OnBoarding For CutShort
+          OnBoarding For CutShort Main Branch
         </a>
       </header>
     </div>
